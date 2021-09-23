f91493c7f658621941a73824488d7f4099830560
Japhet Elias <japhet9@gmail.com>
2021-09-23 19:02:52 +0000

    cleaned up and removed debug / experimentation code and comments

--- client.js
+++ client.js
@@ -16,21 +16,7 @@ const connect = function () {
     console.log("Successfully connected to game server");
     conn.write('Name: JKE');
   });
-  conn.on("connect", () => {
-    // setTimeout(()=> {
-    //   conn.write('Move: up');
-    //   setTimeout(()=> {
-    //     conn.write('Move: up');
-        
-    //   }, 50);
-    // }, 50);
-    // setInterval(()=> {
-    //   conn.write('Move: up');
-    // }, 250);
-    // conn.write('Move: up');
-    // conn.write('Move: up');
-    // conn.write('Move: up');
-  });
+  
   conn.on("data", (data) => {
     // code that does something when receiving data
     console.log("incoming data:", data);
--- constants.js
+++ constants.js
@@ -1,16 +1,19 @@
+// const IP = "localhost";
+// const PORT = "50541";
 const IP = "135.23.223.133";
 const PORT = "50542";
 
 const SIGINT = '\u0003'; // \u0003 maps to ctrl+c input
 
+//movement keys
 const MOVE_UP_KEY = "w";
 const MOVE_LEFT_KEY = "a";
 const MOVE_DOWN_KEY = "s";
 const MOVE_RIGHT_KEY = "d";
 
+//message keys
 const MESSAGE_KEY_1 = "m";
 const MESSAGE_KEY_2 = "n";
-
 
 
 module.exports = {
