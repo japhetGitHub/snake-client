22078dfc73d35e71ea4fc82425143c666eaa8d14
Japhet Elias <japhet9@gmail.com>
2021-09-23 19:02:13 +0000

    refactored multiple ifs into an object that defines the message mappings and based the server writes on these mappings

--- input.js
+++ input.js
@@ -2,34 +2,30 @@ const { SIGINT, MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MESSA
 let connection;
 
 
-// setup interface to handle user input from stdin
-
 const handleUserInput = function (key) {
+  const messageMapping = {
+    [MOVE_UP_KEY]: "Move: up",
+    [MOVE_LEFT_KEY]: "Move: left",
+    [MOVE_DOWN_KEY]: "Move: down",
+    [MOVE_RIGHT_KEY]: "Move: right",
+    [MESSAGE_KEY_1]: "Say: Hsssss",
+    [MESSAGE_KEY_2]: "Say: 1 point to Slytherin House!"
+  };
+  
+  //performs a lookup of the appropriate mapping of the key parameter in the messageMapping object
+  if(Object.keys(messageMapping).includes(key)){
+    connection.write(messageMapping[key])
+  } else {
+    console.log("invalid key - server not signalled");
+  } 
+  
+  //SIGINT is ctrl+c input
   if (key === SIGINT) {
     process.exit();
   }
-  if (key === MOVE_UP_KEY) {
-    connection.write("Move: up");
-  }
-  if (key === MOVE_LEFT_KEY) {
-    connection.write("Move: left");
-  }
-  if (key === MOVE_DOWN_KEY) {
-    connection.write("Move: down");
-  }
-  if (key === MOVE_RIGHT_KEY) {
-    connection.write("Move: right");
-  }
-  if (key === MESSAGE_KEY_1) {
-    connection.write("Say: Hahahahahahah");
-  }
-  if (key === MESSAGE_KEY_2) {
-    connection.write("Say: yeee boii");
-  }
-  
-
 };
 
+// setup interface to handle user input from stdin
 const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;
