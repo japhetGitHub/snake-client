22f9bbd1f2559dc478eba8ad22799a406f25c536
Japhet Elias <japhet9@gmail.com>
2021-09-23 02:25:45 +0000

    added additional connect callback and experimented with with sending messages for successive moves

--- client.js
+++ client.js
@@ -13,7 +13,22 @@ const connect = function () {
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log("Successfully connected to game server");
-    conn.write('Name: JKE')
+    conn.write('Name: JKE');
+  });
+  conn.on("connect", () => {
+    // setTimeout(()=> {
+    //   conn.write('Move: up');
+    //   setTimeout(()=> {
+    //     conn.write('Move: up');
+        
+    //   }, 50);
+    // }, 50);
+    // setInterval(()=> {
+    //   conn.write('Move: up');
+    // }, 250);
+    // conn.write('Move: up');
+    // conn.write('Move: up');
+    // conn.write('Move: up');
   });
   conn.on("data", (data) => {
     // code that does something when the connection is first established
