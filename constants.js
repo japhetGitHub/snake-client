5cec43f6cac7f3eb0400439e48d54448af265ebf
Japhet Elias <japhet9@gmail.com>
2021-09-23 18:15:31 +0000

    refactored more constants out to constants file

--- constants.js
+++ constants.js
@@ -1,7 +1,26 @@
 const IP = "135.23.223.133";
 const PORT = "50542";
 
+const SIGINT = '\u0003'; // \u0003 maps to ctrl+c input
+
+const MOVE_UP_KEY = "w";
+const MOVE_LEFT_KEY = "a";
+const MOVE_DOWN_KEY = "s";
+const MOVE_RIGHT_KEY = "d";
+
+const MESSAGE_KEY_1 = "m";
+const MESSAGE_KEY_2 = "n";
+
+
+
 module.exports = {
   IP,
   PORT,
+  SIGINT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGE_KEY_1,
+  MESSAGE_KEY_2
 };
--- input.js
+++ input.js
@@ -1,30 +1,29 @@
-
+const { SIGINT, MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MESSAGE_KEY_1, MESSAGE_KEY_2 } = require('./constants');
 let connection;
 
 
 // setup interface to handle user input from stdin
 
 const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
+  if (key === SIGINT) {
     process.exit();
   }
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
-  if (key === 'm') {
+  if (key === MESSAGE_KEY_1) {
     connection.write("Say: Hahahahahahah");
   }
-  if (key === 'b') {
+  if (key === MESSAGE_KEY_2) {
     connection.write("Say: yeee boii");
   }
   
