610b33c84371e1ffba83b9bf1ba97a242930f8e4
Japhet Elias <japhet9@gmail.com>
2021-09-23 02:09:41 +0000

    refactored connection function into separate client file and modularized code

--- play.js
+++ play.js
@@ -1,26 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.223.133",
-    port: "50542"
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("connection established");
-  });
-  conn.on("data", (data) => {
-    // code that does something when the connection is first established
-    console.log("incoming data:", data);
-  });
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
